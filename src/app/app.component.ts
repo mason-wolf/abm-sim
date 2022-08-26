b00dab65510c6e80520d0e784a97fb9593a2f64b
Mason Wolf <masonhwolf@gmail.com>
2022-08-26 17:17:28 -0500

    demo build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,7 +52,7 @@ export class AppComponent {
 
     let darkMap = "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
     let openMap = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-    const tiles = L.tileLayer(darkMap, {
+    const tiles = L.tileLayer(openMap, {
       maxZoom: 18,
       minZoom: 3
     });
